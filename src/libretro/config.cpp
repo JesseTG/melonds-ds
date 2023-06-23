1743f03c4f24f40e15f97a3a0f1f4f6b06acd5cb
Jesse Talavera-Greenberg <jesse@jesse.tg>
2023-06-23 17:48:51 -0400

    Note in the options menu that GBA connectivity requires a native BIOS

--- src/libretro/config.cpp
+++ src/libretro/config.cpp
@@ -840,6 +840,7 @@ struct retro_core_option_v2_definition melonds::option_defs_us[] = {
                 nullptr,
                 "If enabled, melonDS will attempt to load a BIOS file from the system directory. "
                 "If no valid BIOS is present, melonDS will fall back to its built-in FreeBIOS. "
+                "Note that GBA connectivity requires a native BIOS. "
                 "If unsure, leave this enabled.",
                 nullptr,
                 "system",
