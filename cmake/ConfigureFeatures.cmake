ef97ce4f777af0841447e5be42dc7de793c4ce0a
Jesse Talavera-Greenberg <jesse@jesse.tg>
2023-07-06 10:46:08 -0400

    Sort the defined options

--- cmake/ConfigureFeatures.cmake
+++ cmake/ConfigureFeatures.cmake
@@ -1,11 +1,11 @@
 # Detects the presence of various libraries or functions and sets the appropriate HAVE_* variables.
 # Other files are used to actually configure the build.
 
+option(ENABLE_DYNAMIC "Build with dynamic library support, if supported by the target." ON)
 option(ENABLE_EGL "Build with EGL support, if supported by the target." OFF)
+option(ENABLE_NETWORKING "Build with networking support, if supported by the target." ON)
 option(ENABLE_THREADS "Build with thread support, if supported by the target." ON)
 option(ENABLE_ZLIB "Build with zlib support, if supported by the target." ON)
-option(ENABLE_NETWORKING "Build with networking support, if supported by the target." ON)
-option(ENABLE_DYNAMIC "Build with dynamic library support, if supported by the target." ON)
 set(OPENGL_PROFILE OpenGL CACHE STRING "OpenGL profile to use if OpenGL is enabled. Valid values are 'OpenGL' and 'OpenGLES'.")
 set_property(CACHE OPENGL_PROFILE PROPERTY STRINGS OpenGL OpenGLES)
 
