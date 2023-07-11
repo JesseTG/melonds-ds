19b4abcd64d9510f6fd7f26795419f5900e58b1f
Jesse Talavera-Greenberg <jesse@jesse.tg>
2023-07-11 10:51:21 -0400

    Consolidate most FetchDependencies logic into a function

--- cmake/FetchDependencies.cmake
+++ cmake/FetchDependencies.cmake
@@ -1,90 +1,41 @@
-if (NOT MELONDS_REPOSITORY_URL)
-    set(
-        MELONDS_REPOSITORY_URL
-        "https://github.com/melonDS-emu/melonDS.git"
-        CACHE STRING
-        "melonDS repository URL. Set this to use a melonDS fork or mirror."
-        FORCE
+function(fetch_dependency name default_url default_tag)
+    string(TOUPPER ${name} VAR_NAME)
+    string(MAKE_C_IDENTIFIER ${VAR_NAME} VAR_NAME)
+
+    if (NOT ${VAR_NAME}_REPOSITORY_URL)
+        set(
+            "${VAR_NAME}_REPOSITORY_URL"
+            "${default_url}"
+            CACHE STRING
+            "${name} repository URL. Set this to use a fork."
+            FORCE
+        )
+    endif ()
+
+    if (NOT ${VAR_NAME}_REPOSITORY_TAG)
+        set(
+            "${VAR_NAME}_REPOSITORY_TAG"
+            "${default_tag}"
+            CACHE STRING
+            "${name} repository commit hash or tag. Set this when using a new version or a custom branch."
+            FORCE
+        )
+    endif ()
+
+    message(STATUS "Using ${name}: ${${VAR_NAME}_REPOSITORY_URL} (ref ${${VAR_NAME}_REPOSITORY_TAG})")
+
+    FetchContent_Declare(
+        ${name}
+        GIT_REPOSITORY "${${VAR_NAME}_REPOSITORY_URL}"
+        GIT_TAG "${${VAR_NAME}_REPOSITORY_TAG}"
     )
-endif ()
 
-if (NOT MELONDS_REPOSITORY_TAG)
-    set(
-        MELONDS_REPOSITORY_TAG
-        "0947e94"
-        CACHE STRING
-        "melonDS repository commit hash or tag. Set this when using a new version of melonDS, or when using a custom branch."
-        FORCE
-    )
-endif ()
+    FetchContent_GetProperties(${name})
+endfunction()
 
-message(STATUS "Using melonDS: ${MELONDS_REPOSITORY_URL} (ref ${MELONDS_REPOSITORY_TAG})")
-
-FetchContent_Declare(
-    melonDS
-    GIT_REPOSITORY "${MELONDS_REPOSITORY_URL}"
-    GIT_TAG "${MELONDS_REPOSITORY_TAG}"
-)
-
-FetchContent_GetProperties(melonDS)
-
-if (NOT LIBRETRO_COMMON_REPOSITORY_URL)
-    set(
-        LIBRETRO_COMMON_REPOSITORY_URL
-        "https://github.com/libretro/libretro-common.git"
-        CACHE STRING
-        "libretro-common repository URL. Set this to use a fork or mirror."
-        FORCE
-    )
-endif ()
-
-if (NOT LIBRETRO_COMMON_REPOSITORY_TAG)
-    set(
-        LIBRETRO_COMMON_REPOSITORY_TAG
-        "10995d5"
-        CACHE STRING
-        "libretro-common repository commit hash or tag. Set this when using a new version or a custom branch."
-        FORCE
-    )
-endif ()
-
-message(STATUS "Using libretro-common: ${LIBRETRO_COMMON_REPOSITORY_URL} (ref ${LIBRETRO_COMMON_REPOSITORY_TAG})")
-
-FetchContent_Declare(
-    libretro-common
-    GIT_REPOSITORY "${LIBRETRO_COMMON_REPOSITORY_URL}"
-    GIT_TAG "${LIBRETRO_COMMON_REPOSITORY_TAG}"
-)
-FetchContent_GetProperties(libretro-common)
-
-if (NOT EMBED_BINARIES_REPOSITORY_URL)
-    set(
-        EMBED_BINARIES_REPOSITORY_URL
-        "https://github.com/andoalon/embed-binaries.git"
-        CACHE STRING
-        "embed-binaries repository URL. Set this to use an embed-binaries fork or mirror."
-        FORCE
-    )
-endif ()
-
-if (NOT EMBED_BINARIES_REPOSITORY_TAG)
-    set(
-        EMBED_BINARIES_REPOSITORY_TAG
-        "21f28ca"
-        CACHE STRING
-        "embed-binaries repository commit hash or tag. Set this when using a new version of embed-binaries, or when using a custom branch."
-        FORCE
-    )
-endif ()
-
-message(STATUS "Using embed-binaries: ${EMBED_BINARIES_REPOSITORY_URL} (ref ${EMBED_BINARIES_REPOSITORY_TAG})")
-
-FetchContent_Declare(
-    embed-binaries
-    GIT_REPOSITORY "${EMBED_BINARIES_REPOSITORY_URL}"
-    GIT_TAG "${EMBED_BINARIES_REPOSITORY_TAG}"
-)
-FetchContent_GetProperties(embed-binaries)
+fetch_dependency("melonDS" "https://github.com/melonDS-emu/melonDS.git" "0947e94")
+fetch_dependency("libretro-common" "https://github.com/libretro/libretro-common.git" "10995d5")
+fetch_dependency("embed-binaries" "https://github.com/andoalon/embed-binaries.git" "21f28ca")
 
 set(CMAKE_MODULE_PATH "${FETCHCONTENT_BASE_DIR}/melonds-src/cmake" "${FETCHCONTENT_BASE_DIR}/embed-binaries-src/cmake" "${CMAKE_MODULE_PATH}")
 set(BUILD_STATIC ON)
