f3a1e2eaadb131370490f43e95e63717634c7f79
Jesse Talavera-Greenberg <jesse@jesse.tg>
2023-07-13 16:14:52 -0400

    Add some constants to screenlayout.hpp

--- src/libretro/screenlayout.hpp
+++ src/libretro/screenlayout.hpp
@@ -36,8 +36,17 @@ namespace melonds {
     /// The native height of a single Nintendo DS screen, in pixels
     constexpr int NDS_SCREEN_HEIGHT = 192;
 
+    template<typename T>
+    constexpr glm::tvec2<T> NDS_SCREEN_SIZE(NDS_SCREEN_WIDTH, NDS_SCREEN_HEIGHT);
+
+    template<typename T>
+    constexpr T NDS_SCREEN_AREA = NDS_SCREEN_WIDTH * NDS_SCREEN_HEIGHT;
+
     // We require a pixel format of RETRO_PIXEL_FORMAT_XRGB8888, so we can assume 4 bytes here
     constexpr int PIXEL_SIZE = 4;
+
+    template<typename T>
+    constexpr T RETRO_MAX_POINTER_LENGTH = 32767;
 
     enum class HybridScreenId {
         Top,
