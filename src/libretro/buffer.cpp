b30ab660da2d9ada5436f39a11d5e3bdd2a5f0ae
Jesse Talavera-Greenberg <jesse@jesse.tg>
2023-07-16 22:26:42 -0400

    Add PixelBuffer

---
+++ src/libretro/buffer.cpp
@@ -0,0 +1,79 @@
+/*
+    Copyright 2023 Jesse Talavera-Greenberg
+
+    melonDS DS is free software: you can redistribute it and/or modify it under
+    the terms of the GNU General Public License as published by the Free
+    Software Foundation, either version 3 of the License, or (at your option)
+    any later version.
+
+    melonDS DS is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with melonDS DS. If not, see http://www.gnu.org/licenses/.
+*/
+
+#include "buffer.hpp"
+
+#include <cstring>
+
+melonds::PixelBuffer::PixelBuffer(glm::uvec2 size) noexcept :
+    size(size),
+    stride(size.x * sizeof(uint32_t)),
+    buffer(new uint32_t[size.x * size.y]) {
+    memset(buffer, 0, size.x * size.y * sizeof(uint32_t));
+}
+
+melonds::PixelBuffer::PixelBuffer(std::nullptr_t) noexcept :
+    size(0, 0),
+    stride(0),
+    buffer(nullptr) {}
+
+melonds::PixelBuffer::~PixelBuffer() noexcept {
+    delete[] buffer;
+}
+
+melonds::PixelBuffer::PixelBuffer(const PixelBuffer& other) noexcept :
+    size(other.size),
+    stride(other.stride),
+    buffer(new uint32_t[other.size.x * other.size.y]) {
+    memcpy(buffer, other.buffer, size.x * size.y * sizeof(uint32_t));
+}
+
+melonds::PixelBuffer::PixelBuffer(PixelBuffer&& other) noexcept :
+    size(other.size),
+    stride(other.stride),
+    buffer(other.buffer) {
+    other.buffer = nullptr;
+}
+
+melonds::PixelBuffer& melonds::PixelBuffer::operator=(const PixelBuffer& other) noexcept {
+    if (this != &other) {
+        delete[] buffer;
+        size = other.size;
+        stride = other.stride;
+        buffer = new uint32_t[size.x * size.y];
+        memcpy(buffer, other.buffer, size.x * size.y * sizeof(uint32_t));
+    }
+    return *this;
+}
+
+melonds::PixelBuffer& melonds::PixelBuffer::operator=(PixelBuffer&& other) noexcept {
+    if (this != &other) {
+        delete[] buffer;
+        size = other.size;
+        stride = other.stride;
+        buffer = other.buffer;
+        other.buffer = nullptr;
+    }
+    return *this;
+}
+
+melonds::PixelBuffer& melonds::PixelBuffer::operator=(std::nullptr_t) noexcept {
+    delete[] buffer;
+    size = glm::uvec2(0, 0);
+    stride = 0;
+    buffer = nullptr;
+    return *this;
+}
---
+++ src/libretro/buffer.hpp
@@ -0,0 +1,67 @@
+/*
+    Copyright 2023 Jesse Talavera-Greenberg
+
+    melonDS DS is free software: you can redistribute it and/or modify it under
+    the terms of the GNU General Public License as published by the Free
+    Software Foundation, either version 3 of the License, or (at your option)
+    any later version.
+
+    melonDS DS is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with melonDS DS. If not, see http://www.gnu.org/licenses/.
+*/
+
+#ifndef MELONDS_DS_BUFFER_HPP
+#define MELONDS_DS_BUFFER_HPP
+
+#include <cstddef>
+#include <cstdint>
+#include <glm/vec2.hpp>
+
+namespace melonds {
+    class PixelBuffer {
+    public:
+        PixelBuffer(glm::uvec2 size) noexcept;
+        PixelBuffer(std::nullptr_t) noexcept;
+        ~PixelBuffer() noexcept;
+        PixelBuffer(const PixelBuffer&) noexcept;
+        PixelBuffer(PixelBuffer&&) noexcept;
+        PixelBuffer& operator=(const PixelBuffer&) noexcept;
+        PixelBuffer& operator=(PixelBuffer&&) noexcept;
+        PixelBuffer& operator=(std::nullptr_t) noexcept;
+
+        [[nodiscard]] uint32_t operator[](glm::uvec2 pos) const noexcept {
+            return buffer[pos.y * stride + pos.x];
+        }
+
+        [[nodiscard]] uint32_t& operator[](glm::uvec2 pos) noexcept {
+            return buffer[pos.y * stride + pos.x];
+        }
+
+        [[nodiscard]] uint32_t* operator[](unsigned row) noexcept {
+            return buffer + row * stride;
+        }
+
+        [[nodiscard]] const uint32_t* operator[](unsigned row) const noexcept {
+            return buffer + row * stride;
+        }
+
+        operator bool() const noexcept { return buffer != nullptr; }
+
+        glm::uvec2 Size() const noexcept { return size; }
+        unsigned Width() const noexcept { return size.x; }
+        unsigned Height() const noexcept { return size.y; }
+        unsigned Stride() const noexcept { return stride; }
+        uint32_t *Buffer() noexcept { return buffer; }
+        const uint32_t *Buffer() const noexcept { return buffer; }
+    private:
+        glm::uvec2 size;
+        unsigned stride;
+        uint32_t *buffer;
+    };
+}
+
+#endif //MELONDS_DS_BUFFER_HPP
