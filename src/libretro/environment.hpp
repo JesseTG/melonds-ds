8e54b35ba826d450f61a3a41b74ca972e6b7af3c
Jesse Talavera-Greenberg <jesse@jesse.tg>
2023-07-20 13:23:26 -0400

    Temporarily add the battery API into my project
    - Just until it's merged in upstream RetroArch

--- src/libretro/environment.hpp
+++ src/libretro/environment.hpp
@@ -21,6 +21,94 @@
 #include <string>
 #include <libretro.h>
 
+// Added temporarily
+#ifndef RETRO_ENVIRONMENT_GET_DEVICE_POWER
+extern "C" {
+/**
+ * Describes how a device is being powered.
+ * @see RETRO_ENVIRONMENT_GET_DEVICE_POWER
+ */
+enum retro_power_state
+{
+    /**
+     * Indicates that the frontend cannot report its power state at this time,
+     * most likely due to a lack of support.
+     *
+     * \c RETRO_ENVIRONMENT_GET_DEVICE_POWER will not return this value;
+     * instead, the environment callback will return \c false.
+     */
+    RETRO_POWERSTATE_UNKNOWN = 0,
+
+    /**
+     * Indicates that the device is running on its battery.
+     * Usually applies to portable devices such as handhelds, laptops, and smartphones.
+     */
+    RETRO_POWERSTATE_DISCHARGING,
+
+    /**
+     * Indicates that the device's battery is currently charging.
+     */
+    RETRO_POWERSTATE_CHARGING,
+
+    /**
+     * Indicates that the device is connected to a power source
+     * and that its battery has finished charging.
+     */
+    RETRO_POWERSTATE_CHARGED,
+
+    /**
+     * Indicates that the device is connected to a power source
+     * and that it does not have a battery.
+     * This usually suggests a desktop computer or a non-portable game console.
+     */
+    RETRO_POWERSTATE_PLUGGED_IN
+};
+
+/**
+ * Indicates that an estimate is not available for the battery level or time remaining,
+ * even if the actual power state is known.
+ */
+#define RETRO_POWERSTATE_NO_ESTIMATE (-1)
+
+/**
+ * Describes the power state of the device running the frontend.
+ * @see RETRO_ENVIRONMENT_GET_DEVICE_POWER
+ */
+struct retro_device_power
+{
+    /**
+     * The current state of the frontend's power usage.
+     */
+    enum retro_power_state state;
+
+    /**
+     * A rough estimate of the amount of time remaining (in seconds)
+     * before the device powers off.
+     * This value depends on a variety of factors,
+     * so it is not guaranteed to be accurate.
+     *
+     * Will be set to \c RETRO_POWERSTATE_NO_ESTIMATE if \c state does not equal \c RETRO_POWERSTATE_DISCHARGING.
+     * May still be set to \c RETRO_POWERSTATE_NO_ESTIMATE if the frontend is unable to provide an estimate.
+     */
+    int seconds;
+
+    /**
+     * The approximate percentage of battery charge,
+     * ranging from 0 to 100 (inclusive).
+     * The device may power off before this reaches 0.
+     *
+     * The user might have configured their device
+     * to stop charging before the battery is full,
+     * so do not assume that this will be 100 in the \c RETRO_POWERSTATE_CHARGED state.
+     */
+    int8_t percent;
+};
+
+#define RETRO_ENVIRONMENT_GET_DEVICE_POWER (77 | RETRO_ENVIRONMENT_EXPERIMENTAL)
+}
+#endif
+
+
 namespace retro {
     constexpr unsigned DEFAULT_ERROR_DURATION = 5000; // in ms
     constexpr unsigned DEFAULT_ERROR_PRIORITY = 3;
