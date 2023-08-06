6ad09eeabc795308978983515a9ec46847d22140
Jesse Talavera-Greenberg <jesse@jesse.tg>
2023-08-06 12:01:09 -0400

    Define the git-related repo variables in a macro

--- cmake/FetchDependencies.cmake
+++ cmake/FetchDependencies.cmake
@@ -1,26 +1,30 @@
-function(fetch_dependency name default_url default_tag)
+macro(define_git_dependency_vars name default_url default_tag)
     string(TOUPPER ${name} VAR_NAME)
     string(MAKE_C_IDENTIFIER ${VAR_NAME} VAR_NAME)
 
     if (NOT ${VAR_NAME}_REPOSITORY_URL)
         set(
-            "${VAR_NAME}_REPOSITORY_URL"
-            "${default_url}"
-            CACHE STRING
-            "${name} repository URL. Set this to use a fork."
-            FORCE
+                "${VAR_NAME}_REPOSITORY_URL"
+                "${default_url}"
+                CACHE STRING
+                "${name} repository URL. Set this to use a fork."
+                FORCE
         )
     endif ()
 
     if (NOT ${VAR_NAME}_REPOSITORY_TAG)
         set(
-            "${VAR_NAME}_REPOSITORY_TAG"
-            "${default_tag}"
-            CACHE STRING
-            "${name} repository commit hash or tag. Set this when using a new version or a custom branch."
-            FORCE
+                "${VAR_NAME}_REPOSITORY_TAG"
+                "${default_tag}"
+                CACHE STRING
+                "${name} repository commit hash or tag. Set this when using a new version or a custom branch."
+                FORCE
         )
     endif ()
+endmacro()
+
+function(fetch_dependency name default_url default_tag)
+    define_git_dependency_vars(${name} ${default_url} ${default_tag})
 
     message(STATUS "Using ${name}: ${${VAR_NAME}_REPOSITORY_URL} (ref ${${VAR_NAME}_REPOSITORY_TAG})")
 
