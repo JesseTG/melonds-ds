5b81a0e1d9cb33dd440dc995f719551c58f072b3
Jesse Talavera-Greenberg <jesse@jesse.tg>
2023-08-06 15:26:33 -0400

    Remove embed_file from utils.cmake
    - It was never implemented or used

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -12,27 +12,3 @@ function(dump_cmake_variables)
         message(DEBUG "${_variableName}=${${_variableName}}")
     endforeach ()
 endfunction()
-
-function(embed_file type source_file destination_directory)
-    if (type STREQUAL TEXT)
-        if (NOT EXISTS source_file)
-            message(SEND_ERROR "Source file ${source_file} doesn't exist.")
-            return()
-        elseif (IS_DIRECTORY source_file)
-            message(SEND_ERROR "Source path ${source_file} exists but is a directory.")
-            return()
-        endif()
-
-        file(MAKE_DIRECTORY destination_directory)
-        file(READ source_contents)
-
-        set(generated_header "
-
-        ")
-    elseif (type STREQUAL BINARY)
-        message(SEND_ERROR "BINARY is not yet implemented.")
-    else ()
-        message(SEND_ERROR "Expected a type of TEXT or BINARY, got ${type}")
-    endif ()
-endfunction()
-
