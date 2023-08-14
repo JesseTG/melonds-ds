da336c9f1804b8d6811d267a841793ef7eb492c6
Jesse Talavera-Greenberg <jesse@jesse.tg>
2023-08-14 19:45:53 -0400

    Implement an iterator over libretro dirents

---
+++ src/libretro/retro/dirent.cpp
@@ -0,0 +1,94 @@
+/*
+    Copyright 2023 Jesse Talavera-Greenberg
+
+    melonDS DS is free software: you can redistribute it and/or modify it under
+    the terms of the GNU General Public License as published by the Free
+    Software Foundation, either version 3 of the License, or (at your option)
+    any later version.
+
+    melonDS DS is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with melonDS DS. If not, see http://www.gnu.org/licenses/.
+*/
+
+
+#include <cstring>
+#include <file/file_path.h>
+#include "dirent.hpp"
+
+retro::file_tree retro::readdir(const std::string &path, bool hidden) noexcept {
+    return file_tree(path, hidden);
+}
+
+retro::file_tree::file_tree(const std::string &path, bool hidden) noexcept: originalPath(path) {
+    dir = retro_opendir_include_hidden(path.c_str(), hidden);
+}
+
+retro::file_tree::~file_tree() noexcept {
+    if (dir) {
+        retro_closedir(dir);
+    }
+}
+
+retro::file_tree::dirent_iterator retro::file_tree::begin() noexcept {
+    return dir ? dirent_iterator(this) : dirent_iterator(nullptr);
+}
+
+retro::file_tree::dirent_iterator retro::file_tree::end() const noexcept {
+    return dirent_iterator(nullptr);
+}
+
+retro::file_tree::dirent_iterator::dirent_iterator(file_tree *ptr) noexcept: m_ptr(ptr) {
+    if (m_ptr) {
+        ++(*this); // Find the first file
+    } else {
+        memset((void *) current.name, 0, sizeof(current.name));
+        current.size = 0;
+        current.flags = 0;
+    }
+}
+
+retro::file_tree::dirent_iterator retro::file_tree::dirent_iterator::operator++(int) noexcept {
+    dirent_iterator tmp = *this;
+    ++(*this);
+    return tmp;
+}
+
+// Go to the next file
+retro::file_tree::dirent_iterator &retro::file_tree::dirent_iterator::operator++() noexcept {
+    if (!m_ptr) {
+        // If this iterator is at its end...
+        return *this;
+    }
+
+    bool done = false;
+    do {
+        bool hasNext = retro_readdir(m_ptr->dir);
+        if (!hasNext) {
+            m_ptr = nullptr;
+            memset((void *) current.name, 0, sizeof(current.name));
+            current.flags = 0;
+            current.size = 0;
+            break;
+        }
+
+        const char *fileName = retro_dirent_get_name(m_ptr->dir);
+        char filePath[PATH_MAX];
+        size_t filePathLength = fill_pathname_join_special(filePath, m_ptr->originalPath.c_str(), fileName,
+                                                           sizeof(filePath));
+        int32_t size = 0;
+        int flags = retro_vfs_stat_impl(filePath, &size);
+        if (is_regular_file(flags)) {
+            // If we've found the next file to return in the outer loop...
+            current.flags = flags;
+            strncpy(const_cast<char *>(current.name), filePath, sizeof(current.name));
+            current.size = size;
+            done = true;
+        }
+    } while (!done);
+
+    return *this;
+}
---
+++ src/libretro/retro/dirent.hpp
@@ -0,0 +1,80 @@
+/*
+    Copyright 2023 Jesse Talavera-Greenberg
+
+    melonDS DS is free software: you can redistribute it and/or modify it under
+    the terms of the GNU General Public License as published by the Free
+    Software Foundation, either version 3 of the License, or (at your option)
+    any later version.
+
+    melonDS DS is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with melonDS DS. If not, see http://www.gnu.org/licenses/.
+*/
+
+#ifndef MELONDS_DS_DIRENT_HPP
+#define MELONDS_DS_DIRENT_HPP
+
+#include <cstdint>
+#include <iterator>
+
+#include <retro_dirent.h>
+#include <vfs/vfs_implementation.h>
+
+namespace retro {
+    constexpr bool is_regular_file(int flags) noexcept {
+        return flags & RETRO_VFS_STAT_IS_VALID && !(flags & (RETRO_VFS_STAT_IS_DIRECTORY | RETRO_VFS_STAT_IS_CHARACTER_SPECIAL));
+    }
+
+    struct dirent {
+        char name[PATH_MAX];
+        int32_t size;
+        int flags;
+
+        dirent() {
+            memset((void *) name, 0, sizeof(name));
+            size = 0;
+            flags = 0;
+        }
+    };
+
+    struct file_tree {
+        using value_type = dirent;
+
+        file_tree(const std::string& path, bool hidden) noexcept;
+        ~file_tree() noexcept;
+
+        struct dirent_iterator {
+            using iterator_category = std::input_iterator_tag;
+            using difference_type = std::ptrdiff_t;
+            using value_type = dirent;
+            using pointer = const value_type*;
+            using reference = const value_type&;
+
+            dirent_iterator(file_tree* ptr) noexcept;
+            dirent_iterator& operator++() noexcept;
+            dirent_iterator operator++(int) noexcept;
+
+            reference operator*() const noexcept { return current; }
+            pointer operator->() const noexcept { return &current; }
+
+            friend bool operator== (const dirent_iterator& a, const dirent_iterator& b) { return a.m_ptr == b.m_ptr; }
+            friend bool operator!= (const dirent_iterator& a, const dirent_iterator& b) { return a.m_ptr != b.m_ptr; }
+        private:
+            file_tree* m_ptr;
+            dirent current;
+        };
+
+        dirent_iterator begin() noexcept;
+        dirent_iterator end() const noexcept;
+    private:
+        RDIR* dir;
+        std::string originalPath;
+    };
+
+    file_tree readdir(const std::string& path, bool hidden) noexcept;
+}
+
+#endif //MELONDS_DS_DIRENT_HPP
