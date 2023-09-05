302eaaff0ca96be4df10c2ed29d7b59a878b142b
Jesse Talavera-Greenberg <jesse@jesse.tg>
2023-09-05 19:39:41 -0400

    Revert "Don't disable zlib's examples from the all target"
    This reverts commit 442bd7997342dcbb21617f98eb2d39e1458591b0.

--- cmake/FetchDependencies.cmake
+++ cmake/FetchDependencies.cmake
@@ -63,3 +63,8 @@ if (TRACY_ENABLE)
     set(TRACY_STATIC ON)
     set(TRACY_ON_DEMAND ON)
 endif()
+
+set_target_properties(example minigzip PROPERTIES EXCLUDE_FROM_ALL TRUE)
+if(HAVE_OFF64_T)
+    set_target_properties(example64 minigzip64 PROPERTIES EXCLUDE_FROM_ALL TRUE)
+endif()
