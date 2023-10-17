e783dd475728871a3fe871a3c5002e5f7cf64cc6
Jesse Talavera-Greenberg <jesse@jesse.tg>
2023-10-17 16:48:20 -0400

    Track memory allocations with Tracy

---
+++ src/libretro/tracy.cpp
@@ -0,0 +1,39 @@
+/*
+    Copyright 2023 Jesse Talavera-Greenberg
+
+    melonDS DS is free software: you can redistribute it and/or modify it under
+    the terms of the GNU General Public License as published by the Free
+    Software Foundation, either version 3 of the License, or (at your option)
+    any later version.
+
+    melonDS DS is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with melonDS DS. If not, see http://www.gnu.org/licenses/.
+*/
+
+#include "tracy.hpp"
+
+void* operator new(std::size_t count)
+{
+    if (count == 0)
+        ++count; // avoid std::malloc(0) which may return nullptr on success
+
+    if (void *ptr = std::malloc(count)) {
+        TracySecureAlloc(ptr, count);
+        return ptr;
+    }
+
+    throw std::bad_alloc{}; // required by [new.delete.single]/3
+}
+
+void operator delete(void* ptr) noexcept
+{
+    if (ptr == nullptr)
+        return;
+
+    TracySecureFree(ptr);
+    std::free(ptr);
+}
