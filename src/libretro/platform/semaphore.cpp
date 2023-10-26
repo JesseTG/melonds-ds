a5fb7a63f34df65ff3163c7e0e762e9ff1004da2
Jesse Talavera-Greenberg <jesse@jesse.tg>
2023-10-26 17:23:46 -0400

    Replace rsemaphore with std::semaphore
    - std::semaphore is actually lighter

--- src/libretro/platform/semaphore.cpp
+++ src/libretro/platform/semaphore.cpp
@@ -14,54 +14,46 @@
     with melonDS DS. If not, see http://www.gnu.org/licenses/.
 */
 
+#include <semaphore>
 #include <Platform.h>
-#include "../../rthreads/rsemaphore.h"
+
+#include "tracy.hpp"
 
 using Platform::Semaphore;
 
+struct Platform::Semaphore {
+    std::counting_semaphore<> semaphore;
+
+    Semaphore() : semaphore(0) {}
+};
+
 Semaphore *Platform::Semaphore_Create()
 {
-#ifdef HAVE_THREADS
-    ssem_t *sem = ssem_new(0);
-    if (sem)
-        return (Semaphore*)sem;
-#endif
-    return nullptr;
+    ZoneScopedN("Platform::Semaphore_Create");
+    return new Semaphore;
 }
 
 void Platform::Semaphore_Reset(Semaphore *sema)
 {
-#ifdef HAVE_THREADS
-    while (ssem_get((ssem_t*)sema) > 0) {
-        ssem_trywait((ssem_t*)sema);
-    }
-#endif
+    ZoneScopedN("Platform::Semaphore_Reset");
+    while (sema->semaphore.try_acquire());
 }
 
 void Platform::Semaphore_Post(Semaphore *sema, int count)
 {
-#ifdef HAVE_THREADS
-    for (int i = 0; i < count; i++)
-    {
-        ssem_signal((ssem_t*)sema);
-    }
-#endif
+    ZoneScopedN("Platform::Semaphore_Post");
+    sema->semaphore.release(count);
 }
 
 void Platform::Semaphore_Wait(Semaphore *sema)
 {
-#ifdef HAVE_THREADS
-    ssem_wait((ssem_t*)sema);
-#endif
+    ZoneScopedN("Platform::Semaphore_Wait");
+    sema->semaphore.acquire();
 }
 
 void Platform::Semaphore_Free(Semaphore *sema)
 {
-
-#ifdef HAVE_THREADS
-    auto *sem = (ssem_t*)sema;
-    if (sem)
-        ssem_free(sem);
-#endif
+    ZoneScopedN("Platform::Semaphore_Free");
+    delete sema;
 }
 
