bf0355feda28d435205f003a3507eea3f90a62b1
Jesse Talavera-Greenberg <jesse@jesse.tg>
2023-12-20 18:48:37 -0500

    Add a new test

--- test/cmake/TestHomebrewSDCards.cmake
+++ test/cmake/TestHomebrewSDCards.cmake
@@ -9,6 +9,16 @@ add_emutest_test(
 )
 
 add_emutest_test(
+    NAME "Homebrew SD card image is not created if disabled and loading a retail ROM"
+    CONTENT "${NDS_ROM}"
+    TEST_SCRIPT "homebrew-sdcard-exists.lua"
+    CORE_OPTION melonds_console_mode="ds"
+    CORE_OPTION melonds_homebrew_sdcard="disabled"
+    CORE_OPTION melonds_homebrew_sync_sdcard_to_host="disabled"
+    WILL_FAIL
+)
+
+add_emutest_test(
     NAME "Homebrew SD card image is created if enabled and loading a homebrew ROM"
     CONTENT "${GODMODE9I_ROM}"
     TEST_SCRIPT "homebrew-sdcard-exists.lua"
