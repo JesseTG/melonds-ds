55e3fe09e385163dd25d0db2f858eed93cac9049
Jesse Talavera-Greenberg <jesse@jesse.tg>
2023-12-20 17:46:06 -0500

    Use the same CMake binary that's running the test suite

--- test/lua/preamble.lua
+++ test/lua/preamble.lua
@@ -1,5 +1,6 @@
 local SYSTEM_FILES = {"ARM7_BIOS", "ARM9_BIOS", "ARM7_DSI_BIOS", "ARM9_DSI_BIOS", "NDS_FIRMWARE", "DSI_FIRMWARE", "DSI_NAND"}
 local emutest_dir = os.getenv("EMUTEST_HOME")
+local cmake = os.getenv("CMAKE")
 local system_dir = emutest_dir .. "/system"
 local save_dir = emutest_dir .. "/savefiles"
 local savestate_directory = emutest_dir .. "/states"
@@ -14,15 +15,15 @@ print("Core system dir:", core_system_dir)
 
 -- We can assume the presence of CMake,
 -- otherwise how would we be running the test suite?
-os.execute(string.format('cmake -E rm -rf "%s" "%s" "%s"', system_dir, save_dir, savestate_directory))
-os.execute(string.format('cmake -E make_directory "%s" "%s" "%s"', core_system_dir, save_dir, savestate_directory))
+os.execute(string.format('"%s" -E rm -rf "%s" "%s" "%s"', cmake, system_dir, save_dir, savestate_directory))
+os.execute(string.format('"%s" -E make_directory "%s" "%s" "%s"', cmake, core_system_dir, save_dir, savestate_directory))
 
 for _, f in ipairs(SYSTEM_FILES) do
     local sysfile = os.getenv(f)
     if sysfile ~= nil and sysfile ~= "" then
         local sysname = os.getenv(f .. "_NAME")
         assert(sysname ~= nil and sysname ~= "")
-        local cmd = string.format('cmake -E copy "%s" "%s/%s"', sysfile, core_system_dir, sysname)
+        local cmd = string.format('"%s" -E copy "%s" "%s/%s"', cmake, sysfile, core_system_dir, sysname)
         -- TODO: For the ARM BIOS files, hardcode the destination filename to "bios7.bin", etc.
         os.execute(cmd)
     end
