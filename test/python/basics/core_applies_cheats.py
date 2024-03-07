35dff56284a8fa65c62c463f1f8f64f366600179
Jesse Talavera <jesse@jesse.tg>
2024-03-06 20:11:55 -0500

    Write some more tests

---
+++ test/python/basics/core_applies_cheats.py
@@ -0,0 +1,19 @@
+from sys import argv
+from libretro import default_session, RETRO_MEMORY_SYSTEM_RAM
+
+with default_session(argv[1]) as session:
+    memory = session.core.get_memory(RETRO_MEMORY_SYSTEM_RAM)
+
+    assert memory is not None
+
+    session.core.cheat_set(0, True, b'02000000-DEADBEEF')
+    # A trivial cheat code with one instruction: 0XXXXXXX-YYYYYYYY,
+    # where XXXXXXX (in this case, 02000000) is the address to write to
+    # and YYYYYYYY (in this case, DEADBEEF) is the value to write.
+
+    assert memory[0:4].tobytes() == b'\xde\xad\xbe\xef'
+
+    session.core.cheat_set(0, False, b'02000000-CAFEBABE')
+
+    assert memory[0:4].tobytes() == b'\xde\xad\xbe\xef'
+    # Passing False to enabled shouldn't apply the cheat
