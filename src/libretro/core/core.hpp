91fe7185ca084b4c3c2ca08398dde7831fdff459
Jesse Talavera <jesse@jesse.tg>
2024-03-13 09:22:59 -0400

    Allow dashe to separate cheat tokens

--- src/libretro/core/core.hpp
+++ src/libretro/core/core.hpp
@@ -143,7 +143,7 @@ namespace MelonDsDs {
         bool _syncClock = false;
         std::unique_ptr<error::ErrorScreen> _messageScreen = nullptr;
         // TODO: Switch to compile time regular expressions (see https://compile-time.re)
-        std::regex _cheatSyntax { "^\\s*[0-9A-Fa-f]{8}([+\\s]*[0-9A-Fa-f]{8})*$", REGEX_OPTIONS };
+        std::regex _cheatSyntax { "^\\s*[0-9A-Fa-f]{8}([+\\s-]*[0-9A-Fa-f]{8})*$", REGEX_OPTIONS };
         std::regex _tokenSyntax { "[0-9A-Fa-f]{8}", REGEX_OPTIONS };
         // This object is meant to be stored in a placement-new'd byte array,
         // so having this flag lets us detect if the core has been initialized
