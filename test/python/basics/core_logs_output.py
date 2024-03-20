59e3cf6ecffd1edba87a77237586c8d750ec9a33
Jesse Talavera <jesse@jesse.tg>
2024-03-20 10:01:30 -0400

    Fix a log test

--- test/python/basics/core_logs_output.py
+++ test/python/basics/core_logs_output.py
@@ -1,12 +1,12 @@
 from typing import cast
 from libretro import Session
-from libretro.api.log import StandardLogger
+from libretro.api.log import UnformattedLogger
 
 import prelude
 
 session: Session
 with prelude.session() as session:
-    log: StandardLogger = cast(StandardLogger, session.log)
+    log: UnformattedLogger = cast(UnformattedLogger, session.log)
     assert log is not None
     assert log.records is not None
     assert len(log.records) > 0
