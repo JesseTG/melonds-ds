fabde5aa6e750608d6b22cf41291319fa333d4c0
Jesse Talavera <jesse@jesse.tg>
2024-03-21 15:01:51 -0400

    Move more tests to libretro.py

---
+++ test/python/firmware/nds_firmware_not_overwritten.py
@@ -0,0 +1,22 @@
+import os
+
+from libretro import Session
+
+import prelude
+
+nds_firmware_path = os.environ["NDS_FIRMWARE"]
+nds_firmware_basename = os.path.basename(nds_firmware_path)
+test_nds_firmware_path = os.path.join(prelude.core_system_dir, nds_firmware_basename)
+
+original_nds_firmware_size = os.stat(nds_firmware_path).st_size
+assert original_nds_firmware_size > 0, f"{nds_firmware_path} is empty"
+
+session: Session
+with prelude.session() as session:
+    for i in range(300):
+        session.core.run()
+
+    test_nds_firmware_size = os.stat(test_nds_firmware_path).st_size
+
+    assert original_nds_firmware_size == test_nds_firmware_size, \
+        f"Expected firmware to be {original_nds_firmware_size} bytes after saving, found {test_nds_firmware_size} bytes"
