14639694820748c001e5e9f5465dc5238f62be50
Jesse Talavera <jesse@jesse.tg>
2024-03-21 15:32:59 -0400

    Migrate more tests to libretro.py

--- test/cmake/Reset.cmake
+++ test/cmake/Reset.cmake
@@ -1,74 +1,56 @@
-
 # See https://github.com/JesseTG/melonds-ds/issues/62
-add_emutest_test(
-        NAME "Core resets when using built-in system files and direct boot (NDS)"
-        CONTENT "${NDS_ROM}"
-        TEST_SCRIPT "no-hang-on-reboot.lua"
-        CORE_OPTION melonds_boot_mode="direct"
-        CORE_OPTION melonds_show_cursor="disabled"
-        CORE_OPTION melonds_sysfile_mode="builtin"
-        CORE_OPTION melonds_console_mode="ds"
+add_python_test(
+    NAME "Core resets when using built-in system files and direct boot (NDS)"
+    TEST_MODULE reset.no_hang_on_reboot
+    CONTENT "${NDS_ROM}"
+    CORE_OPTION "melonds_boot_mode=direct"
+    CORE_OPTION "melonds_console_mode=ds"
+    CORE_OPTION "melonds_show_cursor=disabled"
+    CORE_OPTION "melonds_sysfile_mode=builtin"
 )
 
-add_emutest_test(
-        NAME "Core resets when using native system files and direct boot (NDS)"
-        CONTENT "${NDS_ROM}"
-        TEST_SCRIPT "no-hang-on-reboot.lua"
-        ARM7_BIOS
-        ARM9_BIOS
-        NDS_FIRMWARE
-        CORE_OPTION melonds_firmware_nds_path='melonDS DS/${NDS_FIRMWARE_NAME}'
-        CORE_OPTION melonds_boot_mode="direct"
-        CORE_OPTION melonds_sysfile_mode="native"
-        CORE_OPTION melonds_show_cursor="disabled"
-        CORE_OPTION melonds_console_mode="ds"
+add_python_test(
+    NAME "Core resets when using native system files and direct boot (NDS)"
+    TEST_MODULE reset.no_hang_on_reboot
+    CONTENT "${NDS_ROM}"
+    NDS_SYSFILES
+    CORE_OPTION "melonds_boot_mode=direct"
+    CORE_OPTION "melonds_console_mode=ds"
+    CORE_OPTION "melonds_firmware_nds_path=melonDS DS/${NDS_FIRMWARE_NAME}"
+    CORE_OPTION "melonds_sysfile_mode=native"
 )
 
-add_emutest_test(
-        NAME "Core resets when using native system files and native boot (NDS)"
-        CONTENT "${NDS_ROM}"
-        TEST_SCRIPT "no-hang-on-reboot.lua"
-        ARM7_BIOS
-        ARM9_BIOS
-        NDS_FIRMWARE
-        CORE_OPTION melonds_firmware_nds_path="melonDS DS/${NDS_FIRMWARE_NAME}"
-        CORE_OPTION melonds_boot_mode="native"
-        CORE_OPTION melonds_sysfile_mode="native"
-        CORE_OPTION melonds_show_cursor="disabled"
+add_python_test(
+    NAME "Core resets when using native system files and native boot (NDS)"
+    TEST_MODULE reset.no_hang_on_reboot
+    CONTENT "${NDS_ROM}"
+    NDS_SYSFILES
+    CORE_OPTION "melonds_boot_mode=native"
+    CORE_OPTION "melonds_console_mode=ds"
+    CORE_OPTION "melonds_firmware_nds_path=melonDS DS/${NDS_FIRMWARE_NAME}"
+    CORE_OPTION "melonds_sysfile_mode=native"
 )
 
-add_emutest_test(
-        NAME "Core resets when using native system files and direct boot (DSi)"
-        CONTENT "${NDS_ROM}"
-        TEST_SCRIPT "no-hang-on-reboot.lua"
-        ARM7_BIOS
-        ARM9_BIOS
-        ARM7_DSI_BIOS
-        ARM9_DSI_BIOS
-        DSI_FIRMWARE
-        DSI_NAND
-        CORE_OPTION melonds_firmware_dsi_path="melonDS DS/${DSI_FIRMWARE_NAME}"
-        CORE_OPTION melonds_dsi_nand_path="melonDS DS/${DSI_NAND_NAME}"
-        CORE_OPTION melonds_console_mode="dsi"
-        CORE_OPTION melonds_boot_mode="direct"
-        CORE_OPTION melonds_sysfile_mode="native"
-        CORE_OPTION melonds_show_cursor="disabled"
+add_python_test(
+    NAME "Core resets when using native system files and direct boot (DSi)"
+    TEST_MODULE reset.no_hang_on_reboot
+    CONTENT "${NDS_ROM}"
+    DSI_SYSFILES
+    CORE_OPTION "melonds_boot_mode=direct"
+    CORE_OPTION "melonds_console_mode=dsi"
+    CORE_OPTION "melonds_dsi_nand_path=melonDS DS/${DSI_NAND_NAME}"
+    CORE_OPTION "melonds_firmware_dsi_path=melonDS DS/${DSI_FIRMWARE_NAME}"
+    CORE_OPTION "melonds_sysfile_mode=native"
 )
 
-add_emutest_test(
-        NAME "Core resets when using native system files and native boot (DSi)"
-        CONTENT "${NDS_ROM}"
-        TEST_SCRIPT "no-hang-on-reboot.lua"
-        ARM7_BIOS
-        ARM9_BIOS
-        ARM7_DSI_BIOS
-        ARM9_DSI_BIOS
-        DSI_FIRMWARE
-        DSI_NAND
-        CORE_OPTION melonds_firmware_dsi_path="melonDS DS/${DSI_FIRMWARE_NAME}"
-        CORE_OPTION melonds_console_mode="dsi"
-        CORE_OPTION melonds_dsi_nand_path="melonDS DS/${DSI_NAND_NAME}"
-        CORE_OPTION melonds_boot_mode="native"
-        CORE_OPTION melonds_sysfile_mode="native"
-        CORE_OPTION melonds_show_cursor="disabled"
+add_python_test(
+    NAME "Core resets when using native system files and native boot (DSi)"
+    TEST_MODULE reset.no_hang_on_reboot
+    CONTENT "${NDS_ROM}"
+    DSI_SYSFILES
+    CORE_OPTION "melonds_boot_mode=native"
+    CORE_OPTION "melonds_console_mode=dsi"
+    CORE_OPTION "melonds_dsi_nand_path=melonDS DS/${DSI_NAND_NAME}"
+    CORE_OPTION "melonds_firmware_dsi_path=melonDS DS/${DSI_FIRMWARE_NAME}"
+    CORE_OPTION "melonds_sysfile_mode=native"
 )
