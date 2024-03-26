a941fb398c2b6fd5127d99332756e5133fa1f470
Jesse Talavera <jesse@jesse.tg>
2024-03-25 21:51:58 -0400

    Implement a test for shutting down the core

--- test/python/basics/core_can_shut_down.py
+++ test/python/basics/core_can_shut_down.py
@@ -1,11 +1,11 @@
 import itertools
 from typing import cast
 
-from libretro import Session
+from libretro import Session, CoreShutDownException
 from libretro.api.input import DeviceIdJoypad
-from libretro.api.video import SoftwareVideoState
 
 import prelude
+from libretro.api.video import PillowVideoDriver
 
 
 def generate_input():
@@ -35,8 +35,22 @@ def generate_input():
 
 session: Session
 with prelude.session(input_state=generate_input) as session:
-    video = cast(SoftwareVideoState, session.video)
+    video = cast(PillowVideoDriver, session.video)
+
     for i in range(600):
         session.core.run()
+        # The core should shut down at some point during the test
 
-    assert session.is_shutdown
+    assert False, "session.core should've raised CoreShutDownException and exited the runtime context"
+
+# noinspection PyUnreachableCode
+assert session is not None
+assert session.is_shutdown
+assert session.is_exited
+
+try:
+    session.core.run()
+except CoreShutDownException as e:
+    pass
+
+print("Core shut down successfully")
