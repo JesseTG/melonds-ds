cf5c244039472c65f31244ead1f6c7f629dd03c4
Jesse Talavera <jesse@jesse.tg>
2024-06-04 19:11:46 -0400

    Add `.git` to the melonDS dependency's URL
    - I wonder if this is somehow making `git` choke on the Windows runner?

--- cmake/FetchDependencies.cmake
+++ cmake/FetchDependencies.cmake
@@ -37,7 +37,7 @@ function(fetch_dependency name default_url default_tag)
     FetchContent_GetProperties(${name})
 endfunction()
 
-fetch_dependency(melonDS "https://github.com/melonDS-emu/melonDS" "d48e5f2")
+fetch_dependency(melonDS "https://github.com/melonDS-emu/melonDS.git" "d48e5f2")
 fetch_dependency("libretro-common" "https://github.com/libretro/libretro-common.git" "fce57fd")
 fetch_dependency("embed-binaries" "https://github.com/andoalon/embed-binaries.git" "21f28ca")
 fetch_dependency(glm "https://github.com/g-truc/glm" "33b0eb9")
