4449b4589ec4d8fbde6b84f69e6c9fdb0db49e2d
Bernardo Gomes Negri <b.gomes.negri@gmail.com>
2024-12-01 12:30:34 -0300

    remove unneeded functions

--- src/libretro/libretro.cpp
+++ src/libretro/libretro.cpp
@@ -30,9 +30,7 @@
 #include <compat/strl.h>
 #include <file/file_path.h>
 #include <libretro.h>
-#define RARCH_INTERNAL
 #include <retro_assert.h>
-#undef RARCH_INTERNAL
 #include <retro_miscellaneous.h>
 
 #undef isnan
@@ -65,41 +63,6 @@ namespace MelonDsDs {
     CoreState& Core = *reinterpret_cast<CoreState*>(CoreStateBuffer.data());
 }
 
-void testEqual(const char *mensagem, uint8_t a, uint8_t b) {
-    if (a != b) {
-        retro::error("valores de {} divergem: {} nao e igual a {}", mensagem, a, b);
-    }
-}
-
-void printBuf(const char *buf, size_t len) {
-    const uint8_t *bufa = (const uint8_t *)buf;
-    printf("{ ");
-    for(int i = 0; i < len; i++) {
-        printf("%02X, ", bufa[i]);
-    }
-    printf(" }\n");
-}
-
-void testPacket() {
-    const char *teste = "olá, tudo bem?";
-    MelonDsDs::Packet p{teste, strlen(teste) + 1, 50, 2, true};
-    std::vector<uint8_t> bufBytes = p.ToBuf();
-    const char *buf = (const char *)bufBytes.data();
-    MelonDsDs::Packet np = MelonDsDs::Packet::parsePk(buf, p.Length() + MelonDsDs::HeaderSize);
-
-    retro_assert(np.Length() == p.Length() && p.Length() == strlen(teste) + 1);
-    testEqual("aid dos pacotes", p.Aid(), np.Aid());
-    testEqual("aid esperada", p.Aid(), 2);
-    retro_assert(p.Aid() == np.Aid() && p.Aid() == 2);
-    retro_assert(p.IsReply() == np.IsReply() && p.IsReply());
-    retro_assert(p.Timestamp() == np.Timestamp() && p.Timestamp() == 50);
-    retro_assert(strcmp((const char *)p.Data(), (const char *)np.Data()) == 0 && strcmp((const char *)p.Data(), teste) == 0);
-    std::vector<uint8_t> npBytes = np.ToBuf();
-    printBuf(buf, p.Length() + MelonDsDs::HeaderSize);
-    printBuf((const char *)npBytes.data(), np.Length() + MelonDsDs::HeaderSize);
-    retro_assert(memcmp(buf, (const char *)npBytes.data(), p.Length() + MelonDsDs::HeaderSize) == 0);
-}
-
 PUBLIC_SYMBOL void retro_init(void) {
 #ifdef HAVE_TRACY
     tracy::StartupProfiler();
@@ -116,7 +79,6 @@ PUBLIC_SYMBOL void retro_init(void) {
     memset(MelonDsDs::CoreStateBuffer.data(), 0, MelonDsDs::CoreStateBuffer.size());
     new(&MelonDsDs::CoreStateBuffer) MelonDsDs::CoreState(); // placement-new the CoreState
     retro_assert(MelonDsDs::Core.IsInitialized());
-    testPacket();
 }
 
 PUBLIC_SYMBOL bool retro_load_game(const struct retro_game_info *info) {
